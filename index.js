9b416c8ea43bfe12085a7fe510cb8811550b0c14
Sewookjung <tpdnrqkqh@naver.com>
2019-11-21 15:03:44 +0900

    express testing

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("HI!");
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+  console.log(`Listening: http://localhost:${PORT}`);
+}
+
+function handleHome(req, res) {
+  console.log(req);
+  res.send("Hello from HOME");
+}
+
+function handleProfile(req, res) {
+  res.send("Profile");
+}
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+app.listen(PORT, handleListening);
