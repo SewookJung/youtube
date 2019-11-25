97a1dfd88f57e58834f5f086354f714cf2453be4
Sewookjung <tpdnrqkqh@naver.com>
2019-11-26 00:30:51 +0900

    added localsMiddleware

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { localsMiddleware } from "../middleware/middlewares";
 import userRouter from "../routers/userRouter";
 import videoRouter from "../routers/videoRouter";
 import globalRouter from "../routers/globalRouter";
@@ -17,6 +18,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(localsMiddleware);
 
 //Routers
 app.use(routes.home, globalRouter);
