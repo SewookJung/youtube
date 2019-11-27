8590400f868ea04a68f4c69d388e592fa2b602dc
Sewookjung <tpdnrqkqh@naver.com>
2019-11-27 23:53:50 +0900

    added video DB

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,12 +1,17 @@
-export const home = (req, res) => res.render("home", { pageTitle: "home" });
+import { videos } from "../db";
+
+export const home = (req, res) => {
+  console.log(videos);
+
+  res.render("home", { pageTitle: "home", videos });
+};
 export const search = (req, res) => {
   const {
     query: { term: searchingBy }
   } = req; //Object Destructuring
   res.render("search", { pageTitle: "search", searchingBy });
 };
-export const videos = (req, res) =>
-  res.render("videos", { pageTitle: "videos" });
+
 export const upload = (req, res) =>
   res.render("upload", { pageTitle: "upload" });
 export const videoDetail = (req, res) =>
