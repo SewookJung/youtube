3f648d749271a6ad08c84d508114f71e6b6976e1
Sewookjung <tpdnrqkqh@naver.com>
2019-11-28 00:03:19 +0900

    temporary Database

---
+++ DB/videos.js
@@ -0,0 +1,16 @@
+import { url } from "inspector";
+
+export const videos = [
+  {
+    id: 123123,
+    title: "test",
+    descriptin: "test!",
+    views: 24,
+    videoFile: url("test video.mp4"),
+    creator: {
+      id: 13131313,
+      name: "sewook",
+      email: "tpdnrqkqh@naver.com"
+    }
+  }
+];
