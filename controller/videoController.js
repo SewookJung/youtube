8e70d6a58d4395d4f15ed5f89b55f2510a536d0a
Sewookjung <tpdnrqkqh@naver.com>
2019-11-27 15:27:34 +0900

    added search function

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,7 +1,17 @@
 export const home = (req, res) => res.render("home", { pageTitle: "home" });
-export const search = (req, res) => res.render("search");
-export const videos = (req, res) => res.render("videos");
-export const upload = (req, res) => res.render("upload");
-export const videoDetail = (req, res) => res.render("videoDetail");
-export const editVideo = (req, res) => res.render("editVideo");
-export const deleteVideo = (req, res) => res.render("deleteVideo");
+export const search = (req, res) => {
+  const {
+    query: { term: searchingBy }
+  } = req; //Object Destructuring
+  res.render("search", { pageTitle: "search", searchingBy });
+};
+export const videos = (req, res) =>
+  res.render("videos", { pageTitle: "videos" });
+export const upload = (req, res) =>
+  res.render("upload", { pageTitle: "upload" });
+export const videoDetail = (req, res) =>
+  res.render("videoDetail", { pageTitle: "videoDetail" });
+export const editVideo = (req, res) =>
+  res.render("editVideo", { pageTitle: "editVideo" });
+export const deleteVideo = (req, res) =>
+  res.render("deleteVideo", { pageTitle: "deleteVideo" });
