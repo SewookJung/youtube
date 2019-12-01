4bdc13f16b755d0459049cb6ceecdba4fb8f041f
Sewookjung <tpdnrqkqh@naver.com>
2019-12-01 21:56:28 +0900

    user

--- middleware/middlewares.js
+++ middleware/middlewares.js
@@ -3,5 +3,10 @@ import routes from "../routers/routes";
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "youtube";
   res.locals.routes = routes;
+  res.locals.user = {
+    identificated: true,
+    id: 1
+  };
+
   next();
 };
