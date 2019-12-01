8bcc67851bf5c565e543b01d66d5931cf2846ecb
Sewookjung <tpdnrqkqh@naver.com>
2019-12-01 21:55:29 +0900

    fake database of video

--- DB/videos.js
+++ DB/videos.js
@@ -1,12 +1,63 @@
-import { url } from "inspector";
-
 export const videos = [
   {
     id: 123123,
     title: "test",
-    descriptin: "test!",
+    description: "test!",
     views: 24,
-    videoFile: url("test video.mp4"),
+    videoFile:
+      "http://www.ithinknext.com/mydata/board/files/F201308021823010.mp4",
+    creator: {
+      id: 13131313,
+      name: "sewook",
+      email: "tpdnrqkqh@naver.com"
+    }
+  },
+  {
+    id: 123123,
+    title: "test",
+    description: "test!",
+    views: 24,
+    videoFile:
+      "http://www.ithinknext.com/mydata/board/files/F201308021823010.mp4",
+    creator: {
+      id: 13131313,
+      name: "sewook",
+      email: "tpdnrqkqh@naver.com"
+    }
+  },
+  {
+    id: 123123,
+    title: "test",
+    description: "test!",
+    views: 24,
+    videoFile:
+      "http://www.ithinknext.com/mydata/board/files/F201308021823010.mp4",
+    creator: {
+      id: 13131313,
+      name: "sewook",
+      email: "tpdnrqkqh@naver.com"
+    }
+  },
+  {
+    id: 123123,
+    title: "test",
+    description: "test!",
+    views: 24,
+    videoFile:
+      "http://www.ithinknext.com/mydata/board/files/F201308021823010.mp4",
+    creator: {
+      id: 13131313,
+      name: "sewook",
+      email: "tpdnrqkqh@naver.com"
+    }
+  },
+  {
+    id: 123123,
+    title: "test",
+    description: "test!",
+    views: 24,
+    videoFile:
+      "http://www.ithinknext.com/mydata/board/files/F201308021823010.mp4",
     creator: {
       id: 13131313,
       name: "sewook",
