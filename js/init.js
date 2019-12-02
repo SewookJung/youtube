13e18ef9042c76590914ded6d5afa79f678db9d6
Sewookjung <tpdnrqkqh@naver.com>
2019-12-02 21:11:31 +0900

    added models

--- js/init.js
+++ js/init.js
@@ -1,6 +1,9 @@
-import app from "./app";
 import "../DB/db";
+import "../models/Video";
+import "../models/Comment";
+import app from "./app";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
