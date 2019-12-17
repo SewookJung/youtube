72fde10a06f3c77afc8898bda14afaece8c2bd7e
Sewookjung <tpdnrqkqh@naver.com>
2019-12-17 15:03:50 +0900

    searching video and added regex

--- controller/videoController.js
+++ controller/videoController.js
@@ -10,11 +10,20 @@ export const home = async (req, res) => {
   }
 };
 
-export const search = (req, res) => {
+export const search = async (req, res) => {
   const {
     query: { term: searchingBy }
   } = req; //Object Destructuring
-  res.render("search", { pageTitle: "search", searchingBy });
+  let videos = [];
+  try {
+    videos = await Video.find({
+      title: { $regex: searchingBy, $options: "i" }
+    });
+    console.log(videos);
+  } catch (error) {
+    console.log(error);
+  }
+  res.render("search", { pageTitle: "search", searchingBy, videos });
 };
 
 export const getUpload = (req, res) =>
